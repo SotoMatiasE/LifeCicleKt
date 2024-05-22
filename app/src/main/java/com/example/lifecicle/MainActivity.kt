96708b9033fd47380e6fc8abf830d766ff0b503f
Matias <matias_r12@hotmail.com>
2024-05-22 16:30:28 -0300

    en onResume es ideal para comenzar consultas a base de datos y onPause para pausarlas

--- app/src/main/java/com/example/lifecicle/MainActivity.kt
+++ app/src/main/java/com/example/lifecicle/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.lifecicle
 
+import android.content.Intent
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
+
+    private var mediaPLayer: MediaPlayer? = null
+    private var position: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +24,52 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        findViewById<MaterialButton>(R.id.btnCheck).setOnClickListener {
+            startActivity(Intent(this, DialogActivity::class.java))
+        }
+
+        Log.i("LifeCycle", "onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mediaPLayer = MediaPlayer.create(this, R.raw.ai_2)
+        mediaPLayer?.start()
+        Log.i("LifeCycle", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mediaPLayer?.seekTo(position)
+        mediaPLayer?.start()
+        Log.i("LifeCycle", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mediaPLayer?.pause()
+        if (mediaPLayer != null) {
+            position = mediaPLayer!!.currentPosition
+        }
+        Log.i("LifeCycle", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mediaPLayer?.stop()
+        mediaPLayer?.release()
+        mediaPLayer = null
+        Log.i("LifeCycle", "onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("LifeCycle", "onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("LifeCycle", "onDestroy")
     }
 }
